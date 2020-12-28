ea58ecc37120211a56e54d20fa744f21908d0fda
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-12-28 16:56:34 +0100

    almost done

--- src/BuyWineTest.java
+++ src/BuyWineTest.java
@@ -9,8 +9,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
+/**
+ * TODO javadoc
+ */
 public class BuyWineTest {
 
 	private ControllerLogin loginobj;
@@ -18,33 +21,32 @@ public class BuyWineTest {
 	private ControllerCart cartobj;
 
 	@ParameterizedTest
-	@CsvSource({ "user@user.com, pwd, 20, wine1, producer1, 2015, notes1, 1, grapes1",
-			"employee@employee.com, pwd, 21, wine2, producer2, 2017, notes2, 1, grapes2",
-			"admin@admin.com, pwd, 22, wine3, producer3, 2010, notes3, 1, grapes3", })
-	/*
-	 * "employee@employee.com, pwd", "admin@admin.com, pwd", "asd@asd.com, pwd",
-	 * ", asd", "asd@asd.com," })
-	 */
-	public void procedure(String mail, String pass, int wine_id, String wine_name, String wine_producer, int wine_year,
-			String wine_notes, int wine_quantity, String wine_grapes) {
+	@CsvFileSource(resources = "./testSet.csv", numLinesToSkip = 1)
+	public void procedure(String mail, String pass, int wineId, String wineName, String wine_producer, int wineYear,
+			String wineNotes, int wineQuantity, String wineGrapes) {
 		int permission = login(mail, pass);
 		User user = new User("Placeholder", "Placeholder", mail, pass, permission);
 
 		if (permission > 0) {
-			Wine wine = new Wine(wine_id, wine_name, wine_producer, wine_year, wine_notes, wine_quantity, wine_grapes);
+			Wine wine = new Wine(wineId, wineName, wine_producer, wineYear, wineNotes, wineQuantity, wineGrapes);
 			int addResult = addWine(wine, user);
 
 			if (addResult == 0) {
 				// wine has been added successfully, let's continue
-				System.out.format("Added %d to %s's cart, %d unit(s)\n", wine_id, mail, wine_quantity);
+				System.out.format("Added %d to %s's cart, %d unit(s)\n", wineId, mail, wineQuantity);
 				int submitResult = submitOrder(user);
-				if (submitResult == 0) {
-					System.out.println("let's gooo");
-				}
 
+				if (submitResult == 0) {
+					// Order placed successfully
+					System.out.format("Order has been placed for user %s\n", mail);
+				} else {
+					System.out.format("Order has not been placed for user %s\n", mail);
+				}
+			} else if(addResult == -5){
+				System.out.format("Wine %d doesn't exist.\n", wineId);
 			} else {
 				// addresult < 0 only if the wine is already in the cart
-				fail("Wine " + wine_id + " is already present in the cart for user" + mail);
+				fail("Wine " + wineId + " is already present in the cart for user " + mail);
 			}
 		} else if (permission == 0) {
 			// permission = 0 if the user does not exist.
@@ -149,6 +151,7 @@ public class BuyWineTest {
 
 		try {
 			int result = homepageobj.addToCart(wine, wine.getQuantity());
+
 			switch (result) {
 				case 0:
 					// everything is ok
